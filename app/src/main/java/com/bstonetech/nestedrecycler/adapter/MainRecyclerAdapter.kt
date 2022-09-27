6d0a64baa3910f3c25172596d370892005dc4a5e
Babi Mumba <babimumba243@gmail.com>
2022-09-27 19:11:23 +0200

    add new file

--- app/src/main/java/com/bstonetech/nestedrecycler/adapter/MainRecyclerAdapter.kt
+++ app/src/main/java/com/bstonetech/nestedrecycler/adapter/MainRecyclerAdapter.kt
@@ -1,8 +1,28 @@
 package com.bstonetech.nestedrecycler.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bstonetech.nestedrecycler.R
 import com.bstonetech.nestedrecycler.model.Allcategorie
 
-class MainRecyclerAdapter(private val context: Context, private val allcategorie: List<Allcategorie>):RecyclerView.Adapter<MainRecyclerAdapter.MainviewHolder> {
+class MainRecyclerAdapter(private val context: Context, private val allcategorie: List<Allcategorie>):
+    RecyclerView.Adapter<MainRecyclerAdapter.MainviewHolder>() {
+    class MainviewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainviewHolder {
+        return MainviewHolder((LayoutInflater.from(context).inflate(R.layout)))
+    }
+
+    override fun onBindViewHolder(holder: MainviewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return allcategorie.size
+    }
 }
