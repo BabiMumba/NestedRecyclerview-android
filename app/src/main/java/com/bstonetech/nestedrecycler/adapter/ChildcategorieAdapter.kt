8682673b6352f4cd1773911bbbd52f742f6f603f
Babi Mumba <babimumba243@gmail.com>
2022-09-27 19:51:44 +0200

    ad ne file

--- app/src/main/java/com/bstonetech/nestedrecycler/adapter/ChildcategorieAdapter.kt
+++ app/src/main/java/com/bstonetech/nestedrecycler/adapter/ChildcategorieAdapter.kt
@@ -1,10 +1,27 @@
 package com.bstonetech.nestedrecycler.adapter
 
+import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bstonetech.nestedrecycler.model.CategoryItem
 
-class ChildcategorieAdapter():RecyclerView.Adapter<ChildcategorieAdapter.CatViewHolder> {
+class ChildcategorieAdapter(private val context: Context, private val categoryItem: List<CategoryItem>): RecyclerView.Adapter<ChildcategorieAdapter.CatViewHolder>() {
+
+
     class CatViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return categoryItem.size
     }
 }
