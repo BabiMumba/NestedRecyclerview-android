762859108ff2c594ae2ae09c4751e7a36ebdb91a
Babi Mumba <babimumba243@gmail.com>
2022-09-27 19:06:00 +0200

    add new file

--- app/src/main/java/com/bstonetech/nestedrecycler/adapter/MainRecyclerAdapter.kt
+++ app/src/main/java/com/bstonetech/nestedrecycler/adapter/MainRecyclerAdapter.kt
@@ -1,4 +1,8 @@
 package com.bstonetech.nestedrecycler.adapter
 
-class MainRecyclerAdapter {
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.bstonetech.nestedrecycler.model.Allcategorie
+
+class MainRecyclerAdapter(private val context: Context, private val allcategorie: List<Allcategorie>):RecyclerView.Adapter<MainRecyclerAdapter.MainviewHolder> {
 }
