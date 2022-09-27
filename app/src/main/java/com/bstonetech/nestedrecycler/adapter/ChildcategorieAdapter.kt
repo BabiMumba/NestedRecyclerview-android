ea5f445c7a5885e6033f79221576447302a43f9e
Babi Mumba <babimumba243@gmail.com>
2022-09-27 19:49:42 +0200

    add new file

---
+++ app/src/main/java/com/bstonetech/nestedrecycler/adapter/ChildcategorieAdapter.kt
@@ -0,0 +1,10 @@
+package com.bstonetech.nestedrecycler.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ChildcategorieAdapter():RecyclerView.Adapter<ChildcategorieAdapter.CatViewHolder> {
+    class CatViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+    }
+}
