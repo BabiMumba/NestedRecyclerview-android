e96d408d27ed9c960da80d05f3cc4836733307fe
Babi Mumba <babimumba243@gmail.com>
2022-09-27 19:22:08 +0200

    add new fil

--- app/src/main/java/com/bstonetech/nestedrecycler/MainActivity.kt
+++ app/src/main/java/com/bstonetech/nestedrecycler/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.bstonetech.nestedrecycler
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bstonetech.nestedrecycler.adapter.MainRecyclerAdapter
+import com.bstonetech.nestedrecycler.model.Allcategorie
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+    }
+    private fun setMainCategoriRecycler(allcategorie: List<Allcategorie>){
+        mainrecycler = findViewById(R.id.my_recycler)
+        val layoutManager:RecyclerView.LayoutManager = LinearLayoutManager(this)
+        mainrecycler!!.layoutManager = layoutManager
+        mainRecyclerAdapter = MainRecyclerAdapter(this,allcategorie)
+        mainrecycler!!.adapter = mainRecyclerAdapter
 
     }
 }
