16eafc1996de98a245415fef50461bfea0673a84
Babi Mumba <babimumba243@gmail.com>
2022-09-27 19:53:36 +0200

    add new file

--- app/src/main/java/com/bstonetech/nestedrecycler/adapter/ChildcategorieAdapter.kt
+++ app/src/main/java/com/bstonetech/nestedrecycler/adapter/ChildcategorieAdapter.kt
@@ -1,15 +1,23 @@
 package com.bstonetech.nestedrecycler.adapter
 
 import android.content.Context
+import android.media.Image
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bstonetech.nestedrecycler.R
 import com.bstonetech.nestedrecycler.model.CategoryItem
 
 class ChildcategorieAdapter(private val context: Context, private val categoryItem: List<CategoryItem>): RecyclerView.Adapter<ChildcategorieAdapter.CatViewHolder>() {
 
 
     class CatViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+        var itemImage:ImageView
+        init {
+            itemImage = itemView.findViewById(R.id.imade_row)
+
+        }
 
     }
 
