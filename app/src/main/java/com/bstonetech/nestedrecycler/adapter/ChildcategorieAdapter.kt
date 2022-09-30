d9747fbb37fe67cb003bc8a7699527ea9991d6df
Babi Mumba <babimumba243@gmail.com>
2022-09-30 22:17:06 +0200

    add new file

--- app/src/main/java/com/bstonetech/nestedrecycler/adapter/ChildcategorieAdapter.kt
+++ app/src/main/java/com/bstonetech/nestedrecycler/adapter/ChildcategorieAdapter.kt
@@ -6,31 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bstonetech.nestedrecycler.R
 import com.bstonetech.nestedrecycler.model.CategoryItem
 
-class ChildcategorieAdapter(private val context: Context, private val categoryItem: List<CategoryItem>): RecyclerView.Adapter<ChildcategorieAdapter.CatViewHolder>() {
+class ChildcategorieAdapter(private val context: Context, private var categoryItem: List<CategoryItem>): RecyclerView.Adapter<ChildcategorieAdapter.CatViewHolder>() {
 
 
     class CatViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
         var itemImage:ImageView
         init {
             itemImage = itemView.findViewById(R.id.imade_row)
-
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
         return CatViewHolder(LayoutInflater.from(context).inflate(R.layout.row_cat,parent,false))
-
     }
 
     override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
         holder.itemImage.setImageResource(categoryItem[position].imageUrl)
-    }
 
+    }
     override fun getItemCount(): Int {
         return categoryItem.size
     }
