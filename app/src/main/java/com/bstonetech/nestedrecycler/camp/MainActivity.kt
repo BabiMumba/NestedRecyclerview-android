00642509489cea204d9c7616f407bdb021656a9e
Babi Mumba <babimumba243@gmail.com>
2022-09-30 22:29:26 +0200

    new modification

---
+++ app/src/main/java/com/bstonetech/nestedrecycler/camp/MainActivity.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 Rúben Sousa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bstonetech.nestedrecycler.camp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bstonetech.nestedrecycler.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ParentAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var scrollStateHolder: ScrollStateHolder
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.recyclerView)
+        scrollStateHolder = ScrollStateHolder(savedInstanceState)
+        adapter = ParentAdapter(scrollStateHolder)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        loadItems()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        scrollStateHolder.onSaveInstanceState(outState)
+    }
+
+    private fun loadItems() {
+        val lists = arrayListOf<TitledList>()
+        repeat(20) { listIndex ->
+            val items = arrayListOf<String>()
+            repeat(30) { itemIndex -> items.add(itemIndex.toString()) }
+            lists.add(TitledList("List number $listIndex", items))
+        }
+        adapter.setItems(lists)
+    }
+
+}
